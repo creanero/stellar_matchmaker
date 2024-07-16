44c791717e04172bc226ce962a8cfb462355a3e3
KeneAnumba <bazone2020@gmail.com>
2024-07-16 10:43:12 -0400

    created an __init__ method

--- observation.py
+++ observation.py
@@ -1,3 +1,13 @@
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+class observation:
+
+    def __init__(self):
+        self.mag_limit = 0
+        self.col_limit = 0
+        self.ra_size = 0
+        self.dec_size = 0
 
 def main ():
     pass
