c27401a21abef571bc5c35ad878c7f826fc6fb01
Vidya Venkatesan <46606142+astrovidee@users.noreply.github.com>
2024-07-16 13:25:44 -0500

    changes

--- Stellar_param.py
+++ Stellar_param.py
@@ -1,4 +1,20 @@
-#get new params
-stellar_mass = 0
-stellar_brightness= 0
-stellar_position= 0.0
+#get target star information from user 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+class stellar_param:
+
+    def __init__(self):
+        self.ra = 0 *u.deg
+        self.dec = 0 *u.deg
+        self.G = 0      #filter G from https://www.cosmos.esa.int/web/gaia/dr3-passbands
+        self.bp=0.0     #filter bp 
+        self.rp=0.0     #fileter rp 
+        
+        
+
+def main ():
+    pass
+
+if __name__ == "__main__":
+    main()
