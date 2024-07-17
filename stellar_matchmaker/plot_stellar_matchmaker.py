c002fc0b1ce21ecd7ed5d7dc82a51b921cce5eb5
tyoo37 <astro.taehwa.yoo@gmail.com>
2024-07-17 10:27:09 -0500

    fixed pyproject.toml

---
+++ stellar_matchmaker/plot_stellar_matchmaker.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import SkyCoord, Angle
+
+
+
+def main():
+    pass
+
+def generate_plot(obs, param, results, figsize=10):
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(figsize, figsize))
+    bp_rp = np.array(results['bp_rp'])
+    print(type(bp_rp))
+    vmin=np.nanpercentile(bp_rp,10, method='nearest')
+    vmax=np.nanpercentile(bp_rp,90, method='nearest')
+    plt.scatter(param.ra.to(u.deg), param.dec.to(u.deg), color = "w", marker = "*", s=1000)
+    print("ra = ", param.ra, param.ra.to(u.deg))
+    print("dec = ", param.dec, param.dec.to(u.deg))
+    plt.scatter(results['ra'], results['dec'], c=results['bp_rp'], cmap='RdYlBu_r',s=results['mag_size'], vmin=vmin, vmax=vmax)
+    plt.xlabel('RA (deg)')
+    plt.ylabel('DEC (deg)') 
+    plt.xlim(345.62, 347.62)
+    plt.colorbar(label='BP-RP (mag)')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ stellar_matchmaker/stellar_matchmaker.py
@@ -0,0 +1,96 @@
+import astropy.units as u
+from astropy.coordinates import SkyCoord, Angle
+from astroquery.gaia import Gaia
+from observation import observation
+from Stellar_param import stellar_param
+from plot_stellar_matchmaker import generate_plot
+from Organize import organize
+
+
+def get_inputs():
+    obs = observation()
+    # temporary values for testing and demo
+    obs.set_mag_limit(1 * u.mag)
+    obs.set_col_limit(1 * u.mag)
+    obs.set_ra_size(1 * u.deg)
+    obs.set_dec_size(1 * u.deg)
+
+    param = stellar_param()
+    # values for trappist-1
+    param.set_ra(Angle('23:6:29.37 hours'))
+    param.set_dec(Angle('-5:02:29.04 degrees'))
+    param.set_G(15.62 * u.mag)
+    param.set_bp(19.01 * u.mag)
+    param.set_rp(14.10 * u.mag)
+    
+    return obs, param
+
+
+
+def generate_limits_clause(target, size, name):
+    try: 
+        hi = (target + size).to(u.deg)
+    except:
+        hi = (target + size)
+
+    try:
+        lo = (target - size).to(u.deg)
+    except:
+        lo = (target - size)
+
+    clause = " {0} > {1} ND {0} < {2} ".format(name, lo.value, hi.value)
+
+    return clause
+
+def generate_query(obs, param):
+
+    ra_clause = generate_limits_clause(param.ra, obs.ra_size, "ra")
+    dec_clause = generate_limits_clause(param.dec, obs.dec_size, "dec")
+
+    query = ("SELECT "
+             "ra, dec, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag "
+             "FROM gaiadr3.gaia_source "
+             "WHERE " + ra_clause +
+             "AND " + dec_clause)
+    print(query)
+    return query
+
+def run_query(obs, param):
+
+    query = generate_query(obs, param)
+    job = Gaia.launch_job(query)
+    results = job.get_results()
+    print(results)
+
+    return results
+
+
+def organise_data():
+    pass
+
+
+
+def output_data(obs, param, results):
+    generate_plot(obs, param, results)
+
+    pass
+
+def main():
+    """
+    This function is only called if this program is executed in standalone format.  
+    
+    If we import the functions, this will not be called.
+    """
+
+    # skeleton stucture of the code, will be changed in future version
+    obs, param = get_inputs()
+    result = run_query(obs, param)
+
+    result = organize(obs, param, result)
+    output_data(obs, param, result)
+    
+
+    pass
+
+if __name__ == "__main__":
+    main()
