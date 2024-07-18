a44e7cc3602f0076c675f340b5312cdd416706df
creanero <creanero@gmail.com>
2024-07-18 11:25:18 -0500

    added good reference features, set broad params for test

--- stellar_matchmaker/Organize.py
+++ stellar_matchmaker/Organize.py
@@ -17,9 +17,20 @@ def organize(obs, param, results):
     results["bp_rp"]=results["phot_bp_mean_mag"]- results["phot_rp_mean_mag"] #subract the mags
     results['mag_size']=(obs.mag_limit-results['phot_g_mean_mag'])**2
     np.where(results['mag_size']<1,1,results['mag_size'])
+
+    results = good_ref(obs, param, results)
+    print(results)
     return results
 
-        
+def good_ref(obs, param, results):
+    mag_diff=abs(param.G - results["phot_g_mean_mag"])
+    target_col = (param.bp-param.rp)
+    col_diff=abs(target_col - results["bp_rp"]*u.mag)
+    good_mag=mag_diff <= obs.mag_diff_limit
+    good_col=col_diff <= obs.col_diff_limit
+    results["good"]=np.logical_and(good_mag, good_col)
+
+    return results
     
 def main():
     pass
